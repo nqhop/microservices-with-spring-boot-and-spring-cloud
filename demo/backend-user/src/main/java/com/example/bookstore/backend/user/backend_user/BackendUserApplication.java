82e0d5a15ac870bdc6b04e120bcedfe65f40747d
hop25 <hop35889@gmail.com>
2025-06-20 14:01:05 +0700

    test eureka

---
+++ demo/backend-user/src/main/java/com/example/bookstore/backend/user/backend_user/BackendUserApplication.java
@@ -0,0 +1,17 @@
+package com.example.bookstore.backend.user.backend_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class BackendUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUserApplication.class, args);
+	}
+
+}
